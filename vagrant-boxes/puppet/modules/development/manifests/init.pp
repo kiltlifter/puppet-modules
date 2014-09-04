da7af0fd30a91c5768c6b5d65db39bd18c730ad4
sdouglas <douglas.sean@solute.us>
2014-09-03 17:10:27 -0700

    Added a few selinux tools

--- vagrant-boxes/puppet/modules/development/manifests/init.pp
+++ vagrant-boxes/puppet/modules/development/manifests/init.pp
@@ -1,6 +1,6 @@
 class development {
 	$DEV_FILES	=  '$MODULE_PATH/development/files'
-  include development::java
-  include development::java_build_tools
+  #include development::java
+  #include development::java_build_tools
   include development::version_control
 }
--- vagrant-boxes/puppet/modules/redhat_tools/manifests/utilities.pp
+++ vagrant-boxes/puppet/modules/redhat_tools/manifests/utilities.pp
@@ -16,6 +16,16 @@ class redhat_tools::utilities {
         "httpd-devel",
         "apr-devel",
         "apr-util-devel",
+        "libyaml",
+        "postgresql",
+        "postgresql-devel",
+        "setools",
+        "libsemanage",
+        "libsemanage-devel",
+        "setools-console",
+        "setools-devel",
+        "setools-libs",
+        "policycoreutils-python",
       ]:
         ensure  => present;
     }   
